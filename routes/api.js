6dfae1b2fd09eabd76be971277e38ba8f9aa53d6
Glitch (half-guanaco) <none>
2019-06-23 09:38:38 +0000

    🤘⏰ Checkpoint ./routes/api.js:5024683/664

--- routes/api.js
+++ routes/api.js
@@ -10,14 +10,32 @@
 
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
+const request = require('request-promise');
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const api = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&interval=5min&apikey=' + process.env.APIKEY_AV;
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      
+      const stock = req.query.stock;
+      const like = req.body.like;
+    
+      function getPrice(symbol)
+      {
+        return request.get(api + '&symbol=' + symbol).then((res) => {
+          if(res['Error Message'])
+            return 
+        });
+      }
+    
+      request
+      .get(api + '&symbol=MSFT')
+      .then((res) => {
+        console.log(res);
+      })
+    
     });
     
 };
